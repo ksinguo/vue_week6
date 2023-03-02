3f28d1cac9e2659179b70f1cc4bd27ef89ca7536
ksin <kbs@gmail.com>
2023-03-03 02:09:04 +0800

    '增加product與products'

--- src/views/front/ProductView.vue
+++ src/views/front/ProductView.vue
@@ -1,3 +1,39 @@
 <template>
-    這是單一產品
+    <h1>單一產品細節</h1>
+    <table>
+        <tbody>
+            <tr >
+                <td>{{ product.title }}</td>
+                <td><img :src="product.imageUrl" alt="" width="300">
+                </td>
+            </tr>
+            <tr>
+                <td>{{ product.content }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
+<script>
+const { VITE_APP_URL, VITE_APP_PATH } = import.meta.env
+export default {
+    data() {
+        return {
+            product:{}
+        }
+    },
+    mounted() {
+        // console.log('$route', this.$route.params.id)
+        // console.log('$router', this.$)
+        this.axios.get(`${VITE_APP_URL}/api/${VITE_APP_PATH}/product/${this.$route.params.id}`)
+            .then((res) => {
+                this.product = res.data.product
+                console.log(this.product)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+
+    }
+}
+</script>
--- src/views/front/ProductsView.vue
+++ src/views/front/ProductsView.vue
@@ -1,3 +1,38 @@
 <template>
-    這是商品列表
+    <div>產品列表</div>
+    <table>
+        <tbody>
+            <tr v-for='product in products' :key="product.id">
+                <td>{{product.title}}</td>
+                <td><img :src="product.imageUrl" alt="" width="300">
+                </td>
+                <td>
+                  <RouterLink :to="`/product/${product.id}`">單一產品頁面</RouterLink>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </template>
+<script>
+import { RouterLink, RouterView} from "vue-router";
+const {VITE_APP_URL,VITE_APP_PATH} = import.meta.env
+export default{
+    data() {
+    return {
+      products:[]
+    };
+  },
+    mounted() {
+    this.axios.get(`${VITE_APP_URL}/api/${VITE_APP_PATH}/products/all`)
+    .then((res)=>{
+      console.log(res.data)
+      this.products = res.data.products
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  
+
+  },
+}
+</script>
